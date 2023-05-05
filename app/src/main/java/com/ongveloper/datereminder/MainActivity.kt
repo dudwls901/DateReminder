5ef04f9511f4c0e28f8d1e3320c9d613f37765ec
dudwls901 <dudwls901@naver.com>
2023-05-05 20:30:31 +0900

    [Feat] date, Time, Where, What 뷰 + 데이터 (Reactive)

--- app/src/main/java/com/ongveloper/datereminder/MainActivity.kt
+++ app/src/main/java/com/ongveloper/datereminder/MainActivity.kt
@@ -1,13 +1,62 @@
 package com.ongveloper.datereminder
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.ongveloper.datereminder.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.vm = viewModel
+        binding.lifecycleOwner = this
+
+        initViews()
+        initObservers()
+    }
+
+    private fun initObservers() = with(viewModel){
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED){
+                launch {
+                    selectedDate.collectLatest {
+                        Timber.e("date: ${it}")
+                    }
+                }
+                launch {
+                    selectedTime.collectLatest {
+                        Timber.e("time: ${it}")
+                    }
+                }
+                launch {
+                    selectedWhere.collectLatest {
+                        Timber.e("where: ${it}")
+                    }
+                }
+                launch {
+                    selectedWhat.collectLatest {
+                        Timber.e("what: ${it}")
+                    }
+                }
+            }
+        }
+    }
+
+    private fun initViews() = with(binding){
+        Timber.e("year: ${datePicker.year} month: ${datePicker.month+1} day: ${datePicker.dayOfMonth}")
+        Timber.e("hour: ${timePicker.hour} minute: ${timePicker.minute}")
     }
 }
--- app/src/main/java/com/ongveloper/datereminder/MainViewModel.kt
+++ app/src/main/java/com/ongveloper/datereminder/MainViewModel.kt
@@ -2,8 +2,22 @@ package com.ongveloper.datereminder
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import java.time.LocalDate
+import java.time.LocalTime
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor(): ViewModel() {
+class MainViewModel @Inject constructor() : ViewModel() {
+    var selectedDate = MutableStateFlow<LocalDate>(LocalDate.now())
+    var selectedTime = MutableStateFlow<LocalTime>(LocalTime.now())
+    var selectedWhere = MutableStateFlow<String>("")
+    var selectedWhat = MutableStateFlow<String>("")
+
+    fun onDateChanged(year: Int, monthOfYear: Int, dayOfMonth: Int) {
+        selectedDate.value = LocalDate.of(year, monthOfYear + 1,dayOfMonth)
+    }
+    fun onTimeChanged(hour: Int, minute: Int){
+        selectedTime.value = LocalTime.of(hour, minute)
+    }
 }
